4497ec0a36054f903e00de4ad6a5d9f100aa9152
sober <gongyi964@gmail.com>
2026-08-23 17:11:31 +0800

    feat(web): :sparkles: add frontend styling assets and tooling

--- apps/frontend/web/vite.config.ts
+++ apps/frontend/web/vite.config.ts
@@ -1,7 +1,37 @@
-import babel from '@rolldown/plugin-babel';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+import babel from '@rolldown/plugin-babel';
+import tailwindcss from '@tailwindcss/vite';
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+// 创建 esm 的dirname
+const __dirname = dirname(__filename);
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname, '');
+  const API_PROXY_TARGET = env.VITE_API_PROXY_TARGET || '';
+  return {
+    plugins: [
+      react(),
+      babel({ presets: [reactCompilerPreset()] }),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
+    },
+    server: {
+      proxy: {
+        '^/api': {
+          target: API_PROXY_TARGET,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
